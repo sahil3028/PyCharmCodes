365c0a8898199d181fca28f9bfc46c43c9f1eb54
Sahil Sah <sahil12345work@gmail.com>
2026-01-22 08:25:08 +0530

    completed result program

--- practice_problems/school_result/main.py
+++ practice_problems/school_result/main.py
@@ -1,31 +1,28 @@
 import json
 
 
-with open("studebts.json",'r') as file:
+with open("students.json", 'r') as file:
     data=json.load(file)
 
 students=data["students"]
 
 
 def avg_marks(marks_list):
-    avg=0
-    for m in marks_list["marks"]:
-        avg+=int(m)
+    total=0
+    for m in marks_list:
+        total+=int(m)
+    return total / len(marks_list)
 
-    #marks_list["average"]= avg/len(marks_list["marks"])
-    s["avg"]=(avg / len(marks_list["marks"]))
-
-def grade(avg_list):
-    avg=avg_list["avg"]
+def grade(avg):
 
     if avg >= 80:
-        avg_list["grade"] = "A"
+        return "A"
     elif avg >= 60:
-        avg_list["grade"] = "B"
+        return "B"
     elif avg >= 40:
-        avg_list["grade"] = "C"
+        return "C"
     else:
-        avg_list["grade"] = "F"
+        return "F"
 
 def gen_report(student):
     total_student=len(student)
@@ -45,9 +42,62 @@ def gen_report(student):
     print("Topper:", topper["name"])
     print("Topper Average:", f"{topper['average']:.2f}")
 
-for s in students:
-    avg_marks(s)
-    grade(s)
+# for s in students:
+#     avg_marks(s)
+#     grade(s)
+#
+#
+# gen_report(students)
+def save_to_json(students):
+    data = {"students": students}
+    with open("students.json", "w") as file:
+        json.dump(data, file, indent=4)
 
+while True:
+    print("choose:\n1. Add a student\n2. save and view report\n:3 exit")
+    try:
+        ch=int(input("Enter your choice: "))
+    except ValueError:
+        print("enter a number from the list")
+        continue
 
-gen_report(students)
+    match ch:
+        case 1:
+            while True:
+                name=input("write students name (""exit"")if you wanna stop")
+                if name.lower()=="exit":
+                    break
+                marks=[]
+                for i in range (3):
+                    print(f"enter marks for the sub {i+1}")
+                    try:
+                        m=int(input())
+                    except ValueError:
+                        print("enter a proper no")
+                        continue
+                    if 0<=m<=100:
+                        marks.append(m)
+                    else:
+                        print("enter between 0 to 100")
+                        continue
+
+                avg=avg_marks(marks)
+                grade=grade(avg)
+
+                student = {
+                "name": name,
+                "marks": marks,
+                "average": avg,
+                "grade": grade
+                }
+                students.append(student)
+
+        case 2:
+            gen_report(students)
+            save_to_json(students)
+
+        case 3:
+            print("exitt")
+            break
+        case _:
+            print("invalid choice")
