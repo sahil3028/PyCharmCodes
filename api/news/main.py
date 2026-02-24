2f801333070bdf40152e912b5504b51dd8f803ce
Sahil Sah <sahil12345work@gmail.com>
2026-02-24 17:35:25 +0530

    updated the news email

--- api/news/main.py
+++ api/news/main.py
@@ -1,28 +1,33 @@
 import requests
 import sendEmail
-topic="ps5"
-url=f"https://newsapi.org/v2/everything?q={topic}&from=2026-01-23&sortBy=publishedAt&apiKey=52459937f26d4595aebf8d1282e8f73c&language=en"
 
-#got the whole data in string
-request=requests.get(url)
+topic = "gym"
 
-#got the dict with data
-content=request.json()
-myArticle = ""
+url = (
+    f"https://newsapi.org/v2/everything?"
+    f"q={topic}&"
+    f"from=2026-01-23&"
+    f"sortBy=publishedAt&"
+    f"language=en&"
+    f"apiKey=52459937f26d4595aebf8d1282e8f73c"
+)
+
+response = requests.get(url)
+content = response.json()
+
+# Build HTML email body
+myArticle = f"""
+<h2>📰 Latest {topic.capitalize()} News</h2>
+<p>Here is some political news my script fetched for you.\n I didn't make the UI so i cant deploy it but i gave it topic and and your email, idk what it'll send you</p>
+<hr>
+"""
 
 for article in content["articles"][:5]:
-    myArticle += article["title"] + "\n"+article["description"]+"\n"+article["url"]+"\n\n"
-
-# for article in content["articles"][:5]:
-#     myArticle += f"""
-#     <b>{article['title']}</b><br>
-#     {article['description']}<br>
-#     <a href="{article['url']}">{article['url']}</a><br><br>
-#     """
-
-myArticle=myArticle.encode("utf-8")
-
-
-print(myArticle)
+    myArticle += f"""
+    <h3>{article['title']}</h3>
+    <p>{article.get('description', 'No description available.')}</p>
+    <a href="{article['url']}">Read full article</a>
+    <hr>
+    """
 
 sendEmail.send_email(myArticle)
--- api/news/sendEmail.py
+++ api/news/sendEmail.py
@@ -1,17 +1,26 @@
-import smtplib, ssl
+import smtplib
+import ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-def send_email(mssg):
-    host="smtp.gmail.com"
-    port=465
 
-    username="sahil12345rock@gmail.com"
-    password= "jvaw gzws pcvf mblm"
+def send_email(message_html):
+    host = "smtp.gmail.com"
+    port = 465
 
-    receiver="sahil12345rock@gmail.com"
+    username = "sahil12345rock@gmail.com"
+    password = "jvaw gzws pcvf mblm"
+    receiver = "sahil12345rock@gmail.com"
 
-    context= ssl.create_default_context()
+    msg = MIMEMultipart("alternative")
+    msg["Subject"] = "🗞️ Your Daily News Digest"
+    msg["From"] = username
+    msg["To"] = receiver
 
-    with smtplib.SMTP_SSL(host,port,context=context) as server:
-        server.login(username,password)
-        server.sendmail(username,receiver,mssg)
-#send_email("yooo twin")
+    msg.attach(MIMEText(message_html, "html"))
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, msg.as_string())
